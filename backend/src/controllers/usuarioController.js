d469cb9c042048b49349f1e95b998437e7a4b98c
Rodrigo_Vasquez <rvasquez26@gmail.com>
2024-08-07 23:46:36 -0400

    cambio de objeto a arreglo

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -42,7 +42,7 @@ const usuarioController = {
       if (!usuario) {
         return res.status(404).json({ error: 'Usuario no encontrado' });
       }
-      res.status(200).json(usuario);
+      res.status(200).json([usuario]);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
